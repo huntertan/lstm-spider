78475def7cc96c8db91f0018075ed594e113a18d
hanqing.thq <hanqing.thq@alibaba-inc.com>
2017-11-03 17:29:54 +0800

    finish

--- prediction/PricePredictor.py
+++ prediction/PricePredictor.py
@@ -14,13 +14,14 @@ class PricePredictor:
     output_size=1 #输出维度
     lr = 0.0006 #学习率
     train_x, train_y = [],[] #训练数据集
-    sortedChargeList = []
-
+    sortedChargeList = []  #排序的训练数据集
+    normalize_data = []    #归一化的数据
     dataFile = '/Users/hanqing.thq/github/lstm-spider/chargeInfo.txt'
-    date2Price = {}
-    chargeList = []
-    date2Charge = {}
-
+    date2Price = {}       #日期－每平米的价格映射
+    chargeList = []       #交易价格
+    date2Charge = {}      #日期－交易价格映射
+    mean_price = 0        #均价
+    std_price = 0
     X = tf.placeholder(tf.float32, [None, timeStep, input_size])
     Y = tf.placeholder(tf.float32, [None, timeStep, input_size])
     weights = {
@@ -57,32 +58,34 @@ class PricePredictor:
     def getKey(self, item):
         return item[1]
 
+    # 构造数据
     def buildTrainDataSet(self):
         data = []
         for price in self.sortedChargeList:
             data.append(price[3])
 
-        normalize_data = (data - np.mean(data)) / np.std(data) #标准化
+        self.mean_price = np.mean(data);
+        self.std_price = np.std(data)
+        self.normalize_data = (data - self.mean_price) / self.std_price #标准化
 
-        plt.figure()
-        plt.plot(normalize_data)
-        plt.show()
-
-        normalize_data = normalize_data[:,np.newaxis]  #增加维度
-        for i in range(len(normalize_data)-self.timeStep-1):
-            x=normalize_data[i:i+self.timeStep]
-            y=normalize_data[i+1:i+self.timeStep+1]
+        self.normalize_data = self.normalize_data[:,np.newaxis]  #增加维度
+        for i in range(len(self.normalize_data)-self.timeStep-1):
+            x=self.normalize_data[i:i+self.timeStep]
+            y=self.normalize_data[i+1:i+self.timeStep+1]
             self.train_x.append(x.tolist())
             self.train_y.append(y.tolist())
 
-    def lstm(self):
+    # lstm算法定义
+    def lstm(self, batchSize = None):
+        if batchSize is None :
+            batchSize = self.batchSize
         weightIn = self.weights['in']
         biasesIn = self.biases['in']
         input = tf.reshape(self.X, [-1,self.input_size])
         input_rnn=tf.matmul(input,weightIn)+biasesIn
         input_rnn=tf.reshape(input_rnn,[-1,self.timeStep,self.rnnUnit])  #将tensor转成3维，作为lstm cell的输入
         cell=tf.nn.rnn_cell.BasicLSTMCell(self.rnnUnit)
-        init_state=cell.zero_state(self.batchSize,dtype=tf.float32)
+        init_state=cell.zero_state(batchSize,dtype=tf.float32)
         output_rnn,final_states=tf.nn.dynamic_rnn(cell, input_rnn,initial_state=init_state, dtype=tf.float32)  #output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
         output=tf.reshape(output_rnn,[-1,self.rnnUnit]) #作为输出层的输入
         w_out=self.weights['out']
@@ -90,15 +93,16 @@ class PricePredictor:
         pred=tf.matmul(output,w_out)+b_out
         return pred,final_states
 
+    # 训练模型
     def trainLstm(self) :
         pred,_ = self.lstm()
-        loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(Y, [-1])))
+        loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(self.Y, [-1])))
         train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
         saver = tf.train.Saver(tf.global_variables())
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            # 重复训练10000次
-            for i in range(10000):
+            # 重复训练100次，训练是一个耗时的过程
+            for i in range(100):
                 step = 0
                 start = 0
                 end = start + self.batchSize
@@ -112,12 +116,40 @@ class PricePredictor:
                         print("保存模型：", saver.save(sess, 'stock.model'))
                     step += 1
 
+    def prediction(self):
+        pred, _ = self.lstm(1)  # 预测时只输入[1,time_step,input_size]的测试数据
+        saver = tf.train.Saver(tf.global_variables())
+        with tf.Session() as sess:
+            # 参数恢复
+            module_file = tf.train.latest_checkpoint('/Users/hanqing.thq/github/lstm-spider/prediction/')
+            saver.restore(sess, module_file)
+            # 取训练集最后一行为测试样本. shape=[1,time_step,input_size]
+            prev_seq = self.train_x[-1]
+            predict = []
+            # 得到之后100个预测结果
+            for i in range(100):
+                next_seq = sess.run(pred, feed_dict={self.X: [prev_seq]})
+                predict.append(next_seq[-1])
+                # 每次得到最后一个时间步的预测结果，与之前的数据加在一起，形成新的测试样本
+                prev_seq = np.vstack((prev_seq[1:], next_seq[-1]))
+            # 以折线图表示结果
+            plt.figure()
+            true_price = self.std_price**predict
+            true_price = [price + self.mean_price for price in true_price]
+            plt.plot(list(range(len(self.normalize_data), len(self.normalize_data) + len(predict))), true_price, color='r')
+            plt.show()
+
 predictor = PricePredictor()
-predictor.loadData()
-
+# 模型训练
+# predictor.loadData()
 # sortedChargeList = sorted(predictor.chargeList, key=predictor.getKey, reverse=False)
+# predictor.buildTrainDataSet()
+# predictor.trainLstm()
 
+# 预测－预测前需要先完成模型训练
+predictor.loadData()
 predictor.buildTrainDataSet()
+predictor.prediction()
 
 
 
