f627ac19810efc4a21a1ea3469e2c16eb7972310
hanqing.thq <hanqing.thq@alibaba-inc.com>
2017-11-07 20:31:54 +0800

    finish

--- prediction/PricePredictor.py
+++ prediction/PricePredictor.py
@@ -8,29 +8,29 @@ import tensorflow as tf
 class PricePredictor:
     # lstm param
     timeStep = 20
-    rnnUnit = 10 #hidden layer units
+    hiddenUnitSize = 10 #隐藏层神经元数量
     batchSize = 60  #每一批次训练多少个样例
-    input_size = 1 #输入维度
-    output_size=1 #输出维度
+    inputSize = 1 #输入维度
+    outputSize=1 #输出维度
     lr = 0.0006 #学习率
     train_x, train_y = [],[] #训练数据集
     sortedChargeList = []  #排序的训练数据集
-    normalize_data = []    #归一化的数据
+    normalizeData = []    #归一化的数据
     dataFile = '/Users/hanqing.thq/github/lstm-spider/chargeInfo.txt'
     date2Price = {}       #日期－每平米的价格映射
     chargeList = []       #交易价格
     date2Charge = {}      #日期－交易价格映射
-    mean_price = 0        #均价
-    std_price = 0
-    X = tf.placeholder(tf.float32, [None, timeStep, input_size])
-    Y = tf.placeholder(tf.float32, [None, timeStep, input_size])
+    meanPrice = 0        #均价
+    stdPrice = 0
+    X = tf.placeholder(tf.float32, [None, timeStep, inputSize])
+    Y = tf.placeholder(tf.float32, [None, timeStep, inputSize])
     weights = {
-        'in': tf.Variable(tf.random_normal([input_size, rnnUnit])),
-        'out': tf.Variable(tf.random_normal([rnnUnit, 1]))
+        'in': tf.Variable(tf.random_normal([inputSize, hiddenUnitSize])),
+        'out': tf.Variable(tf.random_normal([hiddenUnitSize, 1]))
     }
 
     biases = {
-        'in': tf.Variable(tf.constant(0.1, shape=[rnnUnit, ])),
+        'in': tf.Variable(tf.constant(0.1, shape=[hiddenUnitSize, ])),
         'out': tf.Variable(tf.constant(0.1, shape=[1, ]))
     }
 
@@ -64,14 +64,14 @@ class PricePredictor:
         for price in self.sortedChargeList:
             data.append(price[3])
 
-        self.mean_price = np.mean(data);
-        self.std_price = np.std(data)
-        self.normalize_data = (data - self.mean_price) / self.std_price #标准化
+        self.meanPrice = np.mean(data);
+        self.stdPrice = np.std(data)
+        self.normalizeData = (data - self.meanPrice) / self.stdPrice #标准化
 
-        self.normalize_data = self.normalize_data[:,np.newaxis]  #增加维度
-        for i in range(len(self.normalize_data)-self.timeStep-1):
-            x=self.normalize_data[i:i+self.timeStep]
-            y=self.normalize_data[i+1:i+self.timeStep+1]
+        self.normalizeData = self.normalizeData[:,np.newaxis]  #增加维度
+        for i in range(len(self.normalizeData)-self.timeStep-1):
+            x=self.normalizeData[i:i+self.timeStep]
+            y=self.normalizeData[i+1:i+self.timeStep+1]
             self.train_x.append(x.tolist())
             self.train_y.append(y.tolist())
 
@@ -81,13 +81,13 @@ class PricePredictor:
             batchSize = self.batchSize
         weightIn = self.weights['in']
         biasesIn = self.biases['in']
-        input = tf.reshape(self.X, [-1,self.input_size])
-        input_rnn=tf.matmul(input,weightIn)+biasesIn
-        input_rnn=tf.reshape(input_rnn,[-1,self.timeStep,self.rnnUnit])  #将tensor转成3维，作为lstm cell的输入
-        cell=tf.nn.rnn_cell.BasicLSTMCell(self.rnnUnit)
-        init_state=cell.zero_state(batchSize,dtype=tf.float32)
-        output_rnn,final_states=tf.nn.dynamic_rnn(cell, input_rnn,initial_state=init_state, dtype=tf.float32)  #output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
-        output=tf.reshape(output_rnn,[-1,self.rnnUnit]) #作为输出层的输入
+        input = tf.reshape(self.X, [-1,self.inputSize])
+        inputRnn=tf.matmul(input,weightIn)+biasesIn
+        inputRnn=tf.reshape(inputRnn,[-1,self.timeStep,self.hiddenUnitSize])  #将tensor转成3维，作为lstm cell的输入
+        cell=tf.nn.rnn_cell.BasicLSTMCell(self.hiddenUnitSize)
+        initState=cell.zero_state(batchSize,dtype=tf.float32)
+        output_rnn,final_states=tf.nn.dynamic_rnn(cell, inputRnn,initial_state=initState, dtype=tf.float32)  #output_rnn是记录lstm每个输出节点的结果，final_states是最后一个cell的结果
+        output=tf.reshape(output_rnn,[-1,self.hiddenUnitSize]) #作为输出层的输入
         w_out=self.weights['out']
         b_out=self.biases['out']
         pred=tf.matmul(output,w_out)+b_out
@@ -96,7 +96,9 @@ class PricePredictor:
     # 训练模型
     def trainLstm(self) :
         pred,_ = self.lstm()
+        #定义损失函数
         loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(self.Y, [-1])))
+        #定义训练模型
         train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
         saver = tf.train.Saver(tf.global_variables())
         with tf.Session() as sess:
@@ -117,13 +119,13 @@ class PricePredictor:
                     step += 1
 
     def prediction(self):
-        pred, _ = self.lstm(1)  # 预测时只输入[1,time_step,input_size]的测试数据
+        pred, _ = self.lstm(1)  # 预测时只输入[1,time_step,inputSize]的测试数据
         saver = tf.train.Saver(tf.global_variables())
         with tf.Session() as sess:
             # 参数恢复
             module_file = tf.train.latest_checkpoint('/Users/hanqing.thq/github/lstm-spider/prediction/')
             saver.restore(sess, module_file)
-            # 取训练集最后一行为测试样本. shape=[1,time_step,input_size]
+            # 取训练集最后一行为测试样本. shape=[1,time_step,inputSize]
             prev_seq = self.train_x[-1]
             predict = []
             # 得到之后100个预测结果
@@ -134,9 +136,9 @@ class PricePredictor:
                 prev_seq = np.vstack((prev_seq[1:], next_seq[-1]))
             # 以折线图表示结果
             plt.figure()
-            true_price = self.std_price**predict
-            true_price = [price + self.mean_price for price in true_price]
-            plt.plot(list(range(len(self.normalize_data), len(self.normalize_data) + len(predict))), true_price, color='r')
+            true_price = self.stdPrice**predict
+            true_price = [price + self.meanPrice for price in true_price]
+            plt.plot(list(range(len(self.normalizeData), len(self.normalizeData) + len(predict))), true_price, color='r')
             plt.show()
 
 predictor = PricePredictor()
